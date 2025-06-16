43535b8286c403016e98a797532b6e0ff86be718
enzomendez315 <enzo.mendez315@gmail.com>
2025-06-16 00:26:58 -0600

    Add requirements and test videos.

--- backend/youtube_download.py
+++ backend/youtube_download.py
@@ -1,2 +1,11 @@
+from pytubefix import YouTube
+from pytubefix.cli import on_progress
+
 url1 = "https://www.youtube.com/watch?v=j_TvWRS2_Hw"
 url2 = "https://www.youtube.com/watch?v=Yvt9vlvuqSQ"
+
+youtube = YouTube(url2, on_progress_callback=on_progress)
+print(youtube.title)
+
+youtube_stream = youtube.streams.get_highest_resolution()
+youtube_stream.download()
