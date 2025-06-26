164032b25a07284ce72b7e043d9112dae41608fc
enzomendez315 <enzo.mendez315@gmail.com>
2025-06-25 20:19:07 -0600

    Implement process_video endpoint.

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,15 @@
+import os
+import pathlib
+import uuid
+
+import boto3
 import modal
-from fastapi import Depends
+from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from pydantic import BaseModel
 
 
-class ProcessVideoRequest():
+class ProcessVideoRequest(BaseModel):
     s3_key: str
 
 
@@ -25,7 +30,12 @@ mount_path = "/root/.cache/torch"
 
 auth_scheme = HTTPBearer()
 
-@app.cls(gpu="L40S", timeout=900, retries=0, scaledown_window=20, secrets=[modal.Secret.from_name("podcast-clipper-secret")], volumes={mount_path: volume})
+
+@app.cls(
+    gpu="L40S", timeout=900, retries=0, scaledown_window=20, secrets=[
+        modal.Secret.from_name("podcast-clipper-secret"), modal.Secret.from_name("aws-secret")
+    ], volumes={mount_path: volume}
+)
 class PodcastClipper:
     @modal.enter()
     def load_model(self):
@@ -34,5 +44,43 @@ class PodcastClipper:
 
     @modal.fastapi_endpoint(method="POST")
     def process_video(self, request: ProcessVideoRequest, token: HTTPAuthorizationCredentials = Depends(auth_scheme)):
-        print(f"Processing video {request.s3_key}")
-        pass
+        #print(f"Processing video {request.s3_key}")
+        s3_key = request.s3_key
+
+        if token.credentials != os.environ["AUTH_TOKEN"]:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, 
+                detail="Incorrect bearer token", 
+                headers={"WWW-Authenticate": "Bearer"}
+            )
+        
+        # Create temporary folder
+        run_id = str(uuid.uuid4())
+        base_dir = pathlib.Path("/tmp") / run_id
+        base_dir.mkdir(parents=True, exist_ok=True)
+
+        # Download video file
+        video_path = base_dir / "input.mp4"
+        s3_client = boto3.client("s3")
+        s3_client.download_file("enzo-podcast-clipper", s3_key, str(video_path))
+
+        print(os.listdir(base_dir))
+
+
+@app.local_entrypoint()
+def main():
+    import requests
+
+    podcast_clipper = PodcastClipper()
+    url = podcast_clipper.process_video.get_web_url()
+    payload = {
+        "s3_key": "test1/50centshort.mp4"
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer 123123"
+    }
+    response = requests.post(url, json=payload, headers=headers)
+    response.raise_for_status()
+    result = response.json()
+    print(result)
